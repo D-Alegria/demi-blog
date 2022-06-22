9a196bba9e2715a9baecc865da97d998f81fbfa0
Oladugba Demilade <demiladeoladugba@gmail.com>
2022-06-22 12:47:54 +0100

    7

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Home = () => {
+
+    const [name, setName] = useState("Demilade");
+    const [age, setAge] = useState(23);
+    const handleClick = () => {
+        setName("Temilade")
+        setAge(21)
+    }
+
     return (
         <div className={"home"}>
             <h2>Home</h2>
+            <p>{name} is {age}< /p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 };
